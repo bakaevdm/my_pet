c5445d4a88cf2555af921f965e113d0455add6cc
bakaevdm <56813818+bakaevdm@users.noreply.github.com>
2020-01-10 21:53:17 +0700

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=lab_l_t',
+    'username' => 'my_pet',
+    'password' => 'tgyhu765',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
